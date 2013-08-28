ce0801d65ea37a61e3ae9391c10ad1715adb16c5
Bruno <bruno.ramos@poli.ufrj.br>
2013-08-27 23:00:15 -0300

    adiciona argumento no dialogo de edicao para definir o tipo de entrada

--- src/br/ufrj/del/geform/app/EditDialog.java
+++ src/br/ufrj/del/geform/app/EditDialog.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.text.Editable;
 import android.widget.EditText;
 import br.ufrj.del.geform.R;
 
@@ -15,6 +16,8 @@ public class EditDialog extends DialogFragment {
 
 	public static final String ARGUMENT_TITLE = "title";
 	public static final String ARGUMENT_VALUE = "value";
+	public static final String ARGUMENT_INPUT_TYPE = "inputType";
+
 
 	private EditText m_input;
 
@@ -29,6 +32,7 @@ public class EditDialog extends DialogFragment {
 
 		final CharSequence title;
 		final CharSequence value;
+		final Integer inputType;
 		final Bundle args = getArguments();
 
 		final CharSequence defaultTitle = getString( R.string.dialog_edit );
@@ -40,14 +44,20 @@ public class EditDialog extends DialogFragment {
 
 			final CharSequence candidateValue = args.getString( ARGUMENT_VALUE );
 			value = candidateValue == null ? defaultValue :  candidateValue;
+
+			inputType = args.getInt( ARGUMENT_INPUT_TYPE );
 		} else {
 			title = defaultTitle;
 			value = defaultValue;
+			inputType = null;
 		}
 
 		builder.setTitle( title );
 
 		m_input = new EditText( getActivity().getApplicationContext() );
+		if( inputType != null ) {
+			m_input.setInputType( inputType );
+		}
 		m_input.setText( value );
 		final int textColor = getResources().getColor( android.R.color.primary_text_light );
 		m_input.setTextColor( textColor );
@@ -90,7 +100,9 @@ public class EditDialog extends DialogFragment {
 	 * @return the input value
 	 */
 	public String getInputValue() {
-		return m_input.getText().toString().trim();
+		final Editable editable = m_input.getText();
+		final String rawText = editable.toString();
+		return rawText.trim();
 	}
 
 	/**
