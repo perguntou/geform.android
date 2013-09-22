a8ef6d5c61a6a766f6280c77271fe8ec28453151
Bruno <bruno.ramos@poli.ufrj.br>
2013-09-22 19:41:36 -0300

    cria classe basica para elementos que possuem ID
    Update issue 34
    a classe IdentifiableBean deve ser usada como base para classes que representam elementos com ID.

---
+++ src/br/ufrj/del/geform/bean/IdentifiableBean.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package br.ufrj.del.geform.bean;
+
+import android.annotation.SuppressLint;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ *
+ */
+@SuppressLint("ParcelCreator")
+public abstract class IdentifiableBean implements Parcelable {
+
+	/*
+	 * initial option ID (not assigned)
+	 */
+	public static final long NO_ID = -1;
+
+	private long m_id;
+
+	protected IdentifiableBean() {
+		this.setId( NO_ID );
+	}
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return m_id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId( final long id ) {
+		this.m_id = id;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see android.os.Parcelable#describeContents()
+	 */
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see android.os.Parcelable#writeToParcel(android.os.Parcel, int)
+	 */
+	@Override
+	public void writeToParcel( Parcel out, int flags ) {
+		out.writeLong( this.getId() );
+	}
+
+	/**
+	 * Constructs a new Option instance from a {@link Parcel}.
+	 * @param in the Parcel
+	 */
+	public IdentifiableBean( Parcel in ) {
+		final long id = in.readLong();
+		this.setId( id );
+	}
+
+}
